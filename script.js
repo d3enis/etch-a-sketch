7fea52a24bf74fadc400805ec141de029065d917
Denis <maxidenis4@gmail.com>
2021-04-15 21:18:11 +0200

    Basic files,styling,functionality,structure

---
+++ script.js
@@ -0,0 +1,26 @@
+let body = document.querySelector("body");
+let div = document.createElement("div");
+div.className = "boxes";
+
+function makeGrid() {
+   for (let i = 0; i < 256; i++) {
+      body.append(div.cloneNode(true))
+   }
+}
+
+
+makeGrid();
+let boxes = document.querySelectorAll(".boxes");
+boxes.forEach(element => element.addEventListener("mouseover", color)
+);
+
+function color() {
+   let r = Math.round(Math.random() * 255);
+   let g = Math.round(Math.random() * 255);
+   let b = Math.round(Math.random() * 255);
+   this.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+}
+
+
+
